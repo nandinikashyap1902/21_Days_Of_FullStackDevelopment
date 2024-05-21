c972724b0d3ece6fcd3e06dd642559eed72b1bda
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-05-21 15:58:06 +0530

    views added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const urlRoute = require('./routes/url')
 const URL = require("./Models/url");
-
+const path = require('path')
 const {MongoDbConnection} = require('./config')
 MongoDbConnection('mongodb://localhost:27017/short-url')
 .then(()=>{
   console.log("mongodb connected")
 })
 const app = express();
+app.set('view engine','ejs')
+app.set("Views",path.resolve("./Views"))
+
 app.use(express.json())
 const PORT = 6000
 app.use('/url',urlRoute)
@@ -26,4 +29,9 @@ app.get("/:shortId",async(req,res)=>{
 console.log(entry)
 res.redirect(entry.redirectURL)
 })
+
+// app.get('/test',async(req,res)=>{
+//   console.log('views is runnning')
+//   return res.render('home')
+// })
 app.listen(PORT,()=>console.log('app is running'))
