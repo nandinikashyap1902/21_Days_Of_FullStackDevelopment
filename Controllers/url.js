3c21120966ae7906f53aa352411702cb05add33f
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-05-24 19:24:46 +0530

    some changes

--- Controllers/url.js
+++ Controllers/url.js
@@ -2,7 +2,6 @@ const shortId= require("shortid")
 const URL = require("../Models/url")
 async function handleNewShortUrl(req,res){
     const body = req.body;
-   
     if(!body.url) return res.status(400).json({error:'url is required'})
         const shortID = shortId()
     await URL.create({
