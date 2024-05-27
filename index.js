91048aafe445d86b20fb11203d667fb811f1c62b
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-05-27 17:37:58 +0530

    auth updated

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const urlRoute = require('./routes/url')
 const staticRoute = require("./routes/staticRouter")
 const userRoute = require('./routes/user')
-
+const cookieParser = require('cookie-parser');
 const URL = require("./Models/url");
 const path = require('path')
 const {MongoDbConnection} = require('./config')
+const{restrictToLoggedinUserOnly} = require('./middlewares/auth')
 MongoDbConnection('mongodb://localhost:27017/short-url')
 .then(()=>{
   console.log("mongodb connected")
@@ -18,9 +19,10 @@ app.set("views",path.resolve("./Views"))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 const PORT = 6001
-app.use('/url',urlRoute)
+app.use('/url',restrictToLoggedinUserOnly,urlRoute)
 app.use("/",staticRoute)
 app.use("/user",userRoute)
+app.use(cookieParser())
 app.get("/url/:shortId",async(req,res)=>{
   const shortId = req.params.shortId;
  const entry= await URL.findOneAndUpdate({
