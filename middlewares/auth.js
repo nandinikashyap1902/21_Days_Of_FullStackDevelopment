5f73db8efb412e09861a3ba4fa8dc74a2335b4a1
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-05-29 14:27:49 +0530

    checkauth updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,14 +15,12 @@ async function restrictToLoggedinUserOnly(req,res,next){
 
 async function checkAuth(req,res,next){
     const userUid = req.cookies?.uid;
-    if(!userUid) return res.redirect("/login")
+
         const user = await getUser(userUid);
-    
-    if(!user) return res.redirect("/login")
         req.user = user;
     next();
 }
 module.exports ={
     restrictToLoggedinUserOnly,
-    checkAuth
+     checkAuth
 }
