6d4a9710e631fbe012fd1fecde808e749a425c56
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-05-30 13:00:18 +0530

    jwt auth implemented

--- Controllers/user.js
+++ Controllers/user.js
@@ -21,9 +21,11 @@ async function UserLogin(req,res){
         return res.render("login",{
     error:"Invalid username or password"
     })
-    const sessionId = uuidv4();
-    setUser(sessionId,user)
-    res.cookie("uid",sessionId)
+    // const sessionId = uuidv4();
+    //setUser(sessionId,user)
+    //res.cookie("uid",sessionId)
+    const token = setUser(user)
+    res.cookie("uid",token)
     return res.redirect("/")
 }
 module.exports={UserSignUp,UserLogin}
--- Service/auth.js
+++ Service/auth.js
@@ -1,10 +1,26 @@
-const sessionIdToUserMap = new Map();
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user)
+// const sessionIdToUserMap = new Map(); //statefull authentication
+const jwt = require("jsonwebtoken")
+const secret = "nandini#45"
+function setUser(user){
+    //sessionIdToUserMap.set(id,user)
+    return jwt.sign(  // stateless
+        {
+            _id:user._id,
+            email:user.email
+        },
+        secret
+    )
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+   // return sessionIdToUserMap.get(id)
+   if(!token) return null;
+   try{
+    return jwt.verify(token,secret)
+   }
+   catch(error){
+    return null;
+   }
 }
 
 module.exports ={
