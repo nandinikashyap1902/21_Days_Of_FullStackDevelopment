b8e6c27dca19be0492506bb764c02ef48d5226f4
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-06-03 20:05:22 +0530

    auth updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,6 @@ async function restrictToLoggedinUserOnly(req,res,next){
         req.user = user;
     next();
 }
-
 async function checkAuth(req,res,next){
     const userUid = req.cookies?.uid;
 
